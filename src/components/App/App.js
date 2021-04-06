571c9f72a830dc336d5704037a0ceb19bd3185d9
gabinwilliams <scoutsnknives@gmail.com>
2021-04-06 16:47:01 -0500

    finished base

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,17 @@ import './App.css';
 function App() {
   // Renders the entire app on the DOM
   // useEffect(() => { getDatGif() }, [])
-  
+  const [ array, setArray ] = useState([]);
+
+  const randomizer = () => {
+    return Math.floor(Math.random() * 10 );
+  }
+
   const [url , setUrl] = useState('');
   const getDatGif = () => {
     axios.get( '/random' ) 
       .then( response => {
-        setUrl( response.data.data[0].images.original.url );
+        setUrl( response.data.data[randomizer()].images.original.url );
         console.log('in app', response.data.data[0].images.original.url);
       }).catch( err => {
         alert( 'you done goofed with the GET route' );
